f5c90638a4f6c3fef581d705a7df53a71081d3ca
Álvaro González <45352512+gonz0wsky@users.noreply.github.com>
2024-12-12 14:16:27 +0100

    chore: set path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+   [
+    'module-resolver',
+    {
+      alias: {
+        '@feature': './src/feature',
+        '@shared': './src/shared',
+        '@core': './src/core',
+        '@assets': './assets',
+      },
+    },
+   ]
+  ],
 };
